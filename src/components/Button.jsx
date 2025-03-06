c6b7a19b811fe049ad35dcb6b499197ed17d26d9
griffinjoshs <leveloutusa@gmail.com>
2025-03-06 10:16:07 -0500

    btn effects

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -22,7 +22,7 @@ export default function Button({
         {...(Component === "button" ? { type } : {})}
         {...(Component === "a" ? { href } : {})}
         onClick={onClick}
-        className={`${classname} bg-primary text-white px-4 py-3 rounded-md text-xl`}
+        className={`${classname} bg-primary text-white px-4 py-3 rounded-md text-xl transition-opacity duration-200 hover:opacity-75`}
         {...props}
       >
         {children}
