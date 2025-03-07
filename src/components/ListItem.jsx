8a62e9a0ade76ba56a80b2286de42ee041a1004d
griffinjoshs <leveloutusa@gmail.com>
2025-03-07 10:30:28 -0500

    images working better

--- src/components/ListItem.jsx
+++ src/components/ListItem.jsx
@@ -4,14 +4,7 @@ import Check from "@/assets/check.png";
 export default function ListItem({ item, collectionName }) {
   return (
     <li className="flex justify-start items-start py-2 text-2xl font-light">
-      <img
-        height="25px"
-        width="25px"
-        src={Check.src}
-        alt="Check"
-        loading="lazy"
-        className="pr-1"
-      />
+     ✅
       <div className="list-content ml-2">
         <h3>{item.data.title}</h3>
         <p>{item.body} </p>
