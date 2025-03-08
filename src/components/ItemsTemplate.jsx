6150bb1f2fa608311a8dce7cd5b103a43bf06327
griffinjoshs <leveloutusa@gmail.com>
2025-03-08 12:13:04 -0500

    grsefrd

--- src/components/ItemsTemplate.jsx
+++ src/components/ItemsTemplate.jsx
@@ -35,9 +35,9 @@ export default async function ItemsTemplate({
   const RenderComponent = ItemComponent || Card;
   return items && items.length > 0 ? (
     <ul className={itemsClass} {...props}>
-      {items.map((item) => (
+      {/* {items.map((item) => (
           <RenderComponent key={item} item={item} itemClass={itemClass} collectionName={collection} />
-      ))}
+      ))} */}
     </ul>
   ) : (
     <p>No items found.</p>
