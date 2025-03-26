c923f5d1e4a418d909b821bd46790496e4508212
griffinjoshs <leveloutusa@gmail.com>
2025-03-26 15:26:09 -0400

    image quality

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,9 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-
 import mdx from '@astrojs/mdx';
-
 import netlify from '@astrojs/netlify';
-
 import tailwindcss from '@tailwindcss/vite';
-
 import react from '@astrojs/react';
-
 import partytown from '@astrojs/partytown';
 
 export default defineConfig({
